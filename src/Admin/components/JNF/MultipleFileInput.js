307e0c4932bf6018947cc9f4b3af8322cb9a34d6
Om Patil <200010036@iitdh.ac.in>
2021-12-22 12:02:37 +0530

    Added multifile UI

--- src/Admin/components/JNF/MultipleFileInput.js
+++ src/Admin/components/JNF/MultipleFileInput.js
@@ -11,15 +11,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 const MultipleFileInput = ({ stateVar, setStateVar, name, label }) => {
   const [fileExceptions, setFileExceptions] = React.useState([])
   const [sizeExceptions, setSizeExceptions] = React.useState([])
-  const fileInput = useRef()
-  // make it invisible
-  // fileInput.current.style.display = 'none'
-  useEffect(() => {
-    if (fileInput){
 
-      fileInput.current.style.display = 'none'
-    }
-  }, [fileInput])
   function addFile (file, stateVar, setStateVar) {
     let localStateVar = stateVar
     let localfileExceptions = []
@@ -51,37 +43,43 @@ const MultipleFileInput = ({ stateVar, setStateVar, name, label }) => {
 
   return (
     <>
-  <Form.Group className="mb-5">
-    <Form.Label>{label}</Form.Label>
-    <br />
-    {stateVar.map((item,index) => (
-      <Chip key={item.name} label={item.name} onDelete={() => {
-        setStateVar(stateVar.filter((_, i) => i !== index))
-        }}
-      />
-    ))}
-    <p className="select-feedback"></p>
-    <Form.Control ref={fileInput} type="file" name={name} multiple={true} accept="application/pdf" onChange={(event) => {addFile(event.currentTarget.files,stateVar,setStateVar)}} />
-    <Button variant="contained" color="primary" onClick={() => {fileInput.current.click()}}>Upload</Button><br />
-    <Form.Text className="text-muted">
-      PDF (Max. 10MB)
-    </Form.Text>
-  </Form.Group>
-  <Dialog open={fileExceptions.length > 0 || sizeExceptions.length > 0 } onClose={()=>{setFileExceptions([]);setSizeExceptions([])}} >
-  <DialogTitle>File Exceptions</DialogTitle>
-  <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {fileExceptions.length > 0 ? `${fileExceptions.join(', ')} is not a PDF file.` : ''}
-            {sizeExceptions.length > 0 ? `${sizeExceptions.join(', ')} is too large.` : ''}
-          </DialogContentText>
-        </DialogContent>
-  <DialogActions>
-          <Button onClick={()=>{setFileExceptions([]);setSizeExceptions([])}} autoFocus>
-            Ok
-          </Button>
-        </DialogActions>
-  </Dialog>
-  </>
+      <Form.Group className="mb-5">
+        <Form.Label>{label}</Form.Label>
+        <div>
+          {stateVar.map((item,index) => (
+            <Chip className='m-1' key={item.name} label={item.name} onDelete={() => {
+              setStateVar(stateVar.filter((_, i) => i !== index))
+              }}
+            />
+          ))}
+        </div>
+        <div className='mt-2 bg-white rounded upload-zone text-center p-2 border border-secondary position-relative' style={{color:'#b3bccd'}} >
+          <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-cloud-arrow-up-fill" viewBox="0 0 16 16">
+            <path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 5.146a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2z"/>
+          </svg>
+          <p className='text-dark'><b>Upload Files</b><br />
+          <span className="text-muted">Click here to select or drag and drop files here</span></p>
+          <Form.Control className='position-absolute top-50 start-50 translate-middle h-100' style={{opacity: 0}} type="file" multiple={true} accept="application/pdf" onChange={(event) => {addFile(event.currentTarget.files,stateVar,setStateVar)}} />
+        </div>
+        <Form.Text className="text-muted">
+          PDF (Max. 10MB)
+        </Form.Text>
+      </Form.Group>
+      <Dialog open={fileExceptions.length > 0 || sizeExceptions.length > 0 } onClose={()=>{setFileExceptions([]);setSizeExceptions([])}} >
+      <DialogTitle>File Exceptions</DialogTitle>
+      <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {fileExceptions.length > 0 ? `${fileExceptions.join(', ')} is not a PDF file.` : ''}
+                {sizeExceptions.length > 0 ? `${sizeExceptions.join(', ')} is too large.` : ''}
+              </DialogContentText>
+            </DialogContent>
+      <DialogActions>
+              <Button onClick={()=>{setFileExceptions([]);setSizeExceptions([])}} autoFocus>
+                Ok
+              </Button>
+            </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
