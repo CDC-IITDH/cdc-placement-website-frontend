4a90598dc93fe03319782b4e02f59aea1982ac60
Abhishek28112002 <pawanmittal2002@gmail.com>
2022-05-28 20:25:56 +0530

    pagination bar

--- src/Admin/components/Details/StudentList.js
+++ src/Admin/components/Details/StudentList.js
@@ -1,13 +1,38 @@
 import useStyles from "./styles";
-import React from "react";
+import React,{ useState} from "react";
 import StudentCard from "./StudentCard";
 import { Grid, Typography } from "@material-ui/core";
+import ReactPaginate from "react-paginate";
+
 
 const StudentList = ({ applicationsInfo, reqJobPosting, token, setError, setShowError, setSuccess, setShowSuccess, setShowLoader,
   openingId, getApplicationsInfo }) => {
   const classes = useStyles();
 
   console.log(applicationsInfo);
+ 
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const usersPerPage = 6;
+  const pagesVisited = pageNumber * usersPerPage;
+
+  // const displayUsers = users
+  //   .slice(pagesVisited, pagesVisited + usersPerPage)
+  //   .map((user) => {
+  //     return (
+  //       <div className="user">
+  //         <h3>{user.firstName}</h3>
+  //         <h3>{user.lastName}</h3>
+  //         <h3>{user.email}</h3>
+  //       </div>
+  //     );
+  //   });
+let  length=applicationsInfo?applicationsInfo.applications.length:0;
+  const pageCount = Math.ceil( length/ usersPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
   return (
     <div className={classes.studentCardContainer}>
@@ -23,9 +48,9 @@ const StudentList = ({ applicationsInfo, reqJobPosting, token, setError, setShow
             </Typography>
           </Grid>
         ) : (
-          applicationsInfo?.applications.map((elem) => {
+          applicationsInfo&& applicationsInfo.applications.slice(pagesVisited, pagesVisited + usersPerPage).map((elem) => {
             return (
-              <Grid key={elem.id} item xs={6} s={6} md={6} lg={4}>
+              <Grid   key={elem.id} item xs={6} s={6} md={6} lg={4}>
                 <StudentCard
                   name={elem.student_details.name}
                   batch={elem.student_details.batch}
@@ -46,12 +71,29 @@ const StudentList = ({ applicationsInfo, reqJobPosting, token, setError, setShow
                   setShowSuccess={setShowSuccess}
                   setShowLoader={setShowLoader}
                   getApplicationsInfo={getApplicationsInfo}
-
                 />
               </Grid>
+              
             );
           })
         )}
+       {applicationsInfo&& (
+         <div>
+ <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
+         </div>
+       )
+  }
+      
       </Grid>
     </div>
   );
