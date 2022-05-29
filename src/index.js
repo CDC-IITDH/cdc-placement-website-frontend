8ab50dc9121842996f6c0a6e470078c687bf87d1
Rohit Gour <rohitgourrg999@gmail.com>
2022-05-29 09:12:03 +0530

    Set up the mocking service on the frontend

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+if (process.env.NODE_ENV === "development") {
+    const { worker } = require("./mocks/browser");
+    worker.start();
+}
+
+ReactDOM.render( < App / > , document.getElementById("root"));
 
 //   //"proxy": "https://cdc-iitdh.herokuapp.com"
