0a52d5ff59d5ef432349af09fb7b05ec6407db29
karthik murakonda <karthik.murakonda14@gmail.com>
2022-06-08 01:00:13 +0530

    added link to opening in searchbar suggestion

--- src/Admin/components/SearchBar/suggestion.js
+++ src/Admin/components/SearchBar/suggestion.js
@@ -1,19 +1,22 @@
-import { React } from "react";
+import { React, useState } from "react";
 import { SvgIcon } from "@material-ui/core";
 import { Work, Ballot } from "@material-ui/icons";
 
 import seachBarStyles from "./seachBarStyles";
+import { Redirect } from "react-router-dom";
 
 const Suggestion = ({ suggestion }) => {
   // text suggestion
   const css = seachBarStyles();
   // ongoing is bool which is true if the suggestion's deadline is in the future
   // console.log(suggestion.deadline_datetime);
+  const [clicked, setClicked] = useState(false);
   const deadline = new Date(suggestion.deadline_datetime);
   const ongoing = deadline > new Date();
   return (
     <>
-      <li className={css.suggestion} >
+      <li className={css.suggestion} onMouseDown={()=>{setClicked(true)}} >
+        {clicked && <Redirect to={`admin/details/placements/${suggestion.id}`} push />} {/* placement to be changed to opening type. */}
           <h5 className={css.suggestion_h5}>
 
           {" "}
